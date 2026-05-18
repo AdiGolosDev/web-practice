96ddde907e212cfe349e25135f2c8fad6306fadc
Adi Golos <golos.adi.03@gmail.com>
2026-05-19 00:18:30 +0200

    added getComputerChoice() function

--- the-odin-project/rock-paper-scissors/rps.js
+++ the-odin-project/rock-paper-scissors/rps.js
@@ -1,1 +1,9 @@
-console.log("Hello World you jackass")
+const choices = ["rock", "paper", "scissors"]
+
+function getComputerChoice() {
+    return choices[Math.floor(Math.random() * 3)]
+    // Math.random() gives quasi-random floating point 0 - 1
+    //Math.floor() rounds result to nearest whole number
+}
+
+
