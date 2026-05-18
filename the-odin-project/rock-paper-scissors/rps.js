49a7f03cc3631875586a5ee107b053af68400f9c
Adi Golos <golos.adi.03@gmail.com>
2026-05-19 00:35:34 +0200

    added getHumanChoice() function

--- the-odin-project/rock-paper-scissors/rps.js
+++ the-odin-project/rock-paper-scissors/rps.js
@@ -1,9 +1,14 @@
-const choices = ["rock", "paper", "scissors"]
+const choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-    return choices[Math.floor(Math.random() * 3)]
+    return choices[Math.floor(Math.random() * 3)];
     // Math.random() gives quasi-random floating point 0 - 1
     //Math.floor() rounds result to nearest whole number
 }
 
+function getHumanChoice() {
+    let humanChoice = prompt("Please choose one of the following: rock, paper, scissors");
+    return humanChoice?.toLowerCase().trim();
+}
 
+console.log(getHumanChoice());
